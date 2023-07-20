a68ab1134fa527c6e05694107fe71b90ae4806fe
muhammadganny <muhammadganny@gmail.com>
2023-07-20 15:54:38 +0500

    design

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,8 +4,8 @@ export default function Main(){
 
     return(
         <main>
-            <h1>Fun fact About React</h1>
-            <ul>
+            <h1 className="main-title" >Fun fact About React</h1>
+            <ul className="main--facts">
                 <li>Was First release in 2013</li>
                 <li>was orignally created by JW</li>
                 <li>has well over 100k stars on git hub</li>
