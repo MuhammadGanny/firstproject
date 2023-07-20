781d45af8c21405b5c3355f114be52e1767e258b
muhammadganny <muhammadganny@gmail.com>
2023-07-20 15:26:57 +0500

    adding logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,12 @@
 import react from "react"
-
+import ReactIcon from "../images/React-icon.svg.png";
 export default function Nav(){
     return(
         <nav>
-            <img/>
-            <h3></h3>
-            <h4></h4>
+            <img src={ReactIcon}/>
+            <h3>ReactFacts</h3>
+            <h4>React Cource - Project 1</h4>
         </nav>
     )
-
     
 }
